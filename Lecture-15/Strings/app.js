5aade96143361b3033e61c9163626081fbb74ebe
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-03-21 19:47:59 +0530

    Lecture 15 codes

---
+++ Lecture-15/Strings/app.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+// let fullName = 'samarth\nvohra';
+// console.log(fullName);
+
+
+let firstName = 'Sharique';
+let lastName = 'Khan';
+
+let greeting = `hi ${firstName} ${lastName} how are you my friend ? `;
+console.log(greeting);
