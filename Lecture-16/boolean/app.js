90e2cebafb4703f7cf0d229aad51ff7cf8140cec
Samarth0606 <officialdevelopersamarth@gmail.com>
2023-03-22 22:00:00 +0530

    lecture 16 codes

---
+++ Lecture-16/boolean/app.js
@@ -0,0 +1,30 @@
+
+
+
+let boolVariable = true;
+// console.log(boolVariable);
+
+let newBool = false;
+// console.log(newBool);
+
+// let one = 'true';
+// let two = 'false';
+
+// let ans1 = (boolVariable == newBool);
+// console.log(ans1);
+
+
+// let ans2 = boolVariable == one;
+// console.log(ans2);
+// console.log(two == newBool);
+
+
+// let ans1 = (boolVariable === newBool);
+// console.log(ans1);
+
+let ans3 = (boolVariable == 1);
+console.log(ans3);
+
+let ans4 = (boolVariable === 1);
+console.log(ans4);
+
---
+++ Lecture-16/stringBasics_Methods/app.js
@@ -0,0 +1,72 @@
+
+
+// let magicNum = 10;
+// magicNum = 'samarth vohra is a web mentor';
+// console.log(magicNum);
+// console.log(magicNum[100]);
+// magicNum[10];
+
+let str;
+str = '      samarthvohra.india@gmail.com       ';
+console.log(str);
+
+// substr method
+
+// console.log(str.substr(2));  
+// console.log(str.substr(2,5));
+// console.log(str.substr(-5,8));
+// console.log(str.substr(-5,-5));
+
+
+// substring method
+
+// console.log(str.substring(2)) ;
+// console.log(str.substring(2 , 5)) ;
+// console.log(str.substring(5 , 2)) ;
+// console.log(str.substring(-5 , 2)) ;
+// console.log(str.substring(-5 , -2)) ;
+
+// indexOf method
+
+// console.log(str.indexOf('a'));
+// console.log(str.indexOf('s'));
+// console.log(str.indexOf('a',2));
+// console.log(str.indexOf('a',4));
+// console.log(str.indexOf('a',12));
+
+
+// 3rd encounter waala question
+// console.log( str.indexOf('a'));    //1st encounter
+// console.log( str.indexOf('a' , str.indexOf('a') + 1 ) );    //2nd encounter
+// console.log( str.indexOf('a' , str.indexOf('a' , str.indexOf('a') + 1 ) +1 ) ); //3rd encounter
+
+// replace
+
+// console.log(str.replace('a' , 'm'));
+
+//replaceAll
+
+// console.log(str.replaceAll('a' , '#'));
+
+// repeat
+// console.log(str.repeat(3));
+
+
+// toUpperCase()
+// console.log(str.toUpperCase());
+
+
+// toLowerCase()
+// console.log(str.toLowerCase());
+
+
+// trim()
+// console.log(str);
+// console.log(str.trim());
+
+
+
+
+
+
+
